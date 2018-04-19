f17efee73c5e1af79dce5608880d3465fb6403db
pc702 <pc702@iucesmag.edu.co>
2018-04-19 18:50:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aula7/quizdosi029214/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.aula7.quizdosi029214;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.aula7.quizdosi029214.Models.User;
+import com.example.aula7.quizdosi029214.Parser.JsonUser;
+import com.example.aula7.quizdosi029214.URL.HttpManager;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
+
+    List<User> userList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.id_pv_data);
+        textView = (TextView) findViewById(R.id.id_tv_data);
+
+        if (isOnLine()){
+            MyTask myTask = new MyTask();
+            myTask.execute("https:jsonplaceholder.typicode.com/users");
+        }
+
+    }
+
+
+    // Metodo para validar la conexion a internet NetworkInfo
+    public Boolean isOnLine(){
+        // Hacer llamado al servicio de conectividad utilizando el ConnectivityManager
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Obtener el estado de la conexion a internet en el dispositivo
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        // Validar el estado obtenido de la conexion
+        if (networkInfo != null){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void processData(){
+
+        for (User user : userList){
+            textView.append(user.getName() + "\n");
+            textView.append(user.getUsername() + "\n");
+            textView.append(user.getEmail() + "\n");
+            textView.append(user.getStreet() + "\n");
+        }
+    }
+
+    // Tarea para traer los datos de post
+    public class MyTask extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String content = null;
+            try {
+                content = HttpManager.getDataJson(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                userList = JsonUser.getData(s);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            processData();
+
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+}
